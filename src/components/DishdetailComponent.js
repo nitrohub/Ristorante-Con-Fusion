8f45feec1cb85a2588087d1dc92cc6ffccf03084
Nitrohub <nitesh.h@somaiya.edu>
2020-12-06 15:25:52 +0530

    Submission

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -10,21 +10,10 @@ class Dishdetail extends Component{
         };
     }
 
-    render(){
-        const dish = this.state.selectedDish;
-        const comment = dish.comments.map((comment)=>{
-            return(
-                <div>
-                    <div>{comment.comment}</div>
-                    <div>-- {comment.author}</div>
-                </div>
-            )
-        })
-        
-        return (
+    renderDish(dish){
+        return(
             
-                <div className="row">
-                    <div className="col-xs-12 col-sm-12 col-md-5 m-1">
+                <div className="col-xs-12 col-sm-12 col-md-5 m-1">
                     <Card>
                     <CardImg width = "100%" object src={dish.image} alt={dish.name} />
                             <CardBody>
@@ -33,10 +22,41 @@ class Dishdetail extends Component{
                             </CardBody>
                     </Card>
                     </div>
-                    <div className="col-xs-12 col-sm-12 col-md-5 col-lg-5 m-1">
-                        <h4>Comments</h4>
-                            {comment}
-                    </div>
+            
+        );
+    }
+
+    renderComments(comments){
+
+        const comment = comments.map((comment)=>{
+            return (
+                <ul className="list-unstyled">
+                    <li>{comment.comment}</li>
+                    <li>-- {comment.author} , {comment.date}</li>
+                </ul>
+            )
+        })
+
+        if(comment!=null){
+            return (
+                <div className="col-xs-12 col-sm-12 col-md-5 col-lg-5 m-1">
+                    <h4>Comments</h4>
+                    {comment}
+                </div>
+            )
+        }else{
+            return(
+                <div></div>
+            )
+        }
+    }
+    render(){
+        const dish = this.state.selectedDish;
+        
+        return (
+                <div className="row">
+                    {this.renderDish(dish)}                      
+                    {this.renderComments(dish.comments)}
                 </div>
             
         );
