1bf43e6d559c820c70aeef6b67041941bb655f16
Nitrohub <nitesh.h@somaiya.edu>
2020-12-17 08:49:14 +0530

    React-route

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { BrowserRouter } from 'react-router-dom';
 // import Dishdetail from './components/DishdetailComponent';
 class App extends Component {
 
 render(){
  return (
+   <BrowserRouter>
     <div>
       <Main />
     </div>
+   </BrowserRouter>
   );
 }
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
+import Home from './HomeComponent';
 import { DISHES } from '../shared/dishes';
 import Dishdetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import {Switch, Route, Redirect} from 'react-router-dom';
 
 
 class Main extends Component{
@@ -11,26 +13,29 @@ class Main extends Component{
   constructor(props){
     super(props);
     this.state = {
-      dishes: DISHES,
-      selectedDish:null
+      dishes: DISHES
     }
   }
 
-  
-  onDishSelect(dishId){
-    // console.log("Selected Dish = "+dishId);
-    this.setState({ selectedDish: dishId });
-}
  
 render(){
+
+  const HomePage = () => {
+    return (
+        <Home/>
+    );
+  }
   const disht = this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish)[0];
   // console.log("Dish TV = ",disht);
  return (
     <div>
       
              <Header />
-             <Menu dishes={this.state.dishes} onClick={(dishId)=> this.onDishSelect(dishId)} /> 
-             <Dishdetail dish={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish)[0]} />
+              <Switch>
+                <Route path="/home" component={Home} />
+                <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes} />} /> /* in this way you can pass in the props inside the menu component */
+                <Redirect to="/home" />
+              </Switch>
              <Footer />
     </div>
   );
