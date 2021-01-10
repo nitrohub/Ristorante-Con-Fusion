23529d137e93594dad766d8021ab445795ecd40d
Nitrohub <nitesh.h@somaiya.edu>
2021-01-10 14:08:59 +0530

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,17 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Leaders } from './leaders';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState,
+        combineReducers({
+            dishes : Dishes,
+            comments : Comments,
+            promotions : Promotions,
+            leaders : Leaders
+        })
     );
 
     return store;
